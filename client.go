40d917cae71f0d307ef01454d3dffe997b7acbdd
rkritchat <rkritchat@gmail.com>
2022-03-25 13:10:53 +0700

    refactor code

---
+++ client.go
@@ -0,0 +1,56 @@
+package csvtogo
+
+import (
+	"os"
+	"unicode/utf8"
+)
+
+type Client[T any] struct {
+	CsvToStruct[T]
+}
+
+func NewClient[T any](csvFile string, ops ...Options) (*Client[T], error) {
+	option := _defaultOps
+	if ops != nil {
+		//validate ops
+		options, err := validateOps(ops[0])
+		if err != nil {
+			return nil, err
+		}
+		option = *options
+	}
+
+	//validate csvFile
+	f, err := os.Open(csvFile)
+	if err != nil {
+		return nil, err
+	}
+	_ = f.Close()
+
+	return &Client[T]{
+		CsvToStruct[T]{
+			targetCSV: csvFile,
+			ops:       option,
+			outsChan:  make(chan []T, 1),
+			outChan:   make(chan T, 1),
+			endChan:   make(chan bool, 1),
+			nextChan:  make(chan bool, 1),
+			errChan:   make(chan error),
+			run:       true,
+		},
+	}, nil
+}
+
+func validateOps(ops Options) (*Options, error) {
+	if utf8.RuneCountInString(string(ops.Comma)) == 0 {
+		return nil, csvCommaIsRequired
+	}
+	if len(ops.SkipCol) > 0 {
+		m := make(map[int]int)
+		for _, val := range ops.SkipCol {
+			m[val] = val
+		}
+		ops.skipper = m
+	}
+	return &ops, nil
+}
--- csvtogo.go
+++ csvtogo.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"reflect"
 	"strconv"
-	"unicode/utf8"
 )
 
 const (
@@ -21,10 +20,6 @@ var _defaultOps = Options{
 }
 
 var csvCommaIsRequired = errors.New("ops.Comma is required")
-
-type Client[T any] struct {
-	CsvToStruct[T]
-}
 
 type CsvToStruct[T any] struct {
 	targetCSV string
@@ -44,52 +39,6 @@ type Options struct {
 	ReplaceWith map[string]string
 	Comma       rune
 	skipper     map[int]int
-}
-
-func NewClient[T any](csvFile string, ops ...Options) (*Client[T], error) {
-	option := _defaultOps
-	if ops != nil {
-		//validate ops
-		options, err := validateOps(ops[0])
-		if err != nil {
-			return nil, err
-		}
-		option = *options
-	}
-
-	//validate csvFile
-	f, err := os.Open(csvFile)
-	if err != nil {
-		return nil, err
-	}
-	_ = f.Close()
-
-	return &Client[T]{
-		CsvToStruct[T]{
-			targetCSV: csvFile,
-			ops:       option,
-			outsChan:  make(chan []T, 1),
-			outChan:   make(chan T, 1),
-			endChan:   make(chan bool, 1),
-			nextChan:  make(chan bool, 1),
-			errChan:   make(chan error),
-			run:       true,
-		},
-	}, nil
-}
-
-func validateOps(ops Options) (*Options, error) {
-	if utf8.RuneCountInString(string(ops.Comma)) == 0 {
-		return nil, csvCommaIsRequired
-	}
-	if len(ops.SkipCol) > 0 {
-		m := make(map[int]int)
-		for _, val := range ops.SkipCol {
-			m[val] = val
-		}
-		ops.skipper = m
-	}
-	return &ops, nil
 }
 
 func (c *CsvToStruct[T]) ToList() *CsvToStruct[T] {
