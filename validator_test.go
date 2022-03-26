539df9e7bcfad5242220072a8722cf403ad466a2
rkritchat <rkritchat@gmail.com>
2022-03-26 21:11:23 +0700

    add unittest on validator

--- validator_test.go
+++ validator_test.go
@@ -15,9 +15,9 @@ func Test_validateStruct(t *testing.T) {
 		Age       int    `json:"age"`
 	}
 	tt := []struct {
-		name        string
-		t           Student
-		expectedErr error
+		name      string
+		t         Student
+		expectedE error
 	}{
 		{
 			name: "should return err is nil when struct is valid",
@@ -26,7 +26,7 @@ func Test_validateStruct(t *testing.T) {
 				Lastname:  strings.Repeat("e", 9),
 				Age:       1,
 			},
-			expectedErr: nil,
+			expectedE: nil,
 		},
 		{
 			name: "should return err when Firstname is more than max",
@@ -35,7 +35,7 @@ func Test_validateStruct(t *testing.T) {
 				Lastname:  strings.Repeat("e", 9),
 				Age:       28,
 			},
-			expectedErr: errors.New("value of Firstname at row 1 is invalid, value length must less than or equal 10, but got: 11"),
+			expectedE: errors.New("value of Firstname at row 1 is invalid, value length must less than or equal 10, but got: 11"),
 		},
 		{
 			name: "should return err when Lastname is less than min",
@@ -44,15 +44,15 @@ func Test_validateStruct(t *testing.T) {
 				Lastname:  "",
 				Age:       28,
 			},
-			expectedErr: errors.New("value of Lastname at row 1 is invalid, value length must more than or equal 1, but got: 0"),
+			expectedE: errors.New("value of Lastname at row 1 is invalid, value length must more than or equal 1, but got: 0"),
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			e := validateStruct[Student](tc.t, 1)
-			if fmt.Sprintf("%v", tc.expectedErr) != fmt.Sprintf("%v", e) {
-				t.Errorf("must:%v, but got: %v", tc.expectedErr, e)
+			if fmt.Sprintf("%v", tc.expectedE) != fmt.Sprintf("%v", e) {
+				t.Errorf("must:%v, but got: %v", tc.expectedE, e)
 			}
 		})
 	}
@@ -60,14 +60,14 @@ func Test_validateStruct(t *testing.T) {
 
 func Test_checkMin(t *testing.T) {
 	type Student struct {
-		Firstname string `json:"firstname" max:"10" min:"1"`
-		Lastname  string `json:"lastname" max:"10" min:"1"`
+		Firstname string `json:"firstname" min:"1"`
+		Lastname  string `json:"lastname" min:"1"`
 		Age       int    `json:"age"`
 	}
 	tt := []struct {
-		name        string
-		t           Student
-		expectedErr error
+		name      string
+		t         Student
+		expectedE error
 	}{
 		{
 			name: "should return nil when struct is valid",
@@ -76,7 +76,7 @@ func Test_checkMin(t *testing.T) {
 				Lastname:  strings.Repeat("e", 9),
 				Age:       1,
 			},
-			expectedErr: nil,
+			expectedE: nil,
 		},
 		{
 			name: "should return nil when struct is valid and some field doesn't have min tag",
@@ -85,14 +85,14 @@ func Test_checkMin(t *testing.T) {
 				Lastname:  strings.Repeat("e", 9),
 				Age:       1,
 			},
-			expectedErr: nil,
+			expectedE: nil,
 		},
 	}
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			e := checkMin[Student](tc.t, 1, 1, reflect.ValueOf(&tc.t).Elem())
-			if fmt.Sprintf("%v", tc.expectedErr) != fmt.Sprintf("%v", e) {
-				t.Errorf("must:%v, but got: %v", tc.expectedErr, e)
+			if fmt.Sprintf("%v", tc.expectedE) != fmt.Sprintf("%v", e) {
+				t.Errorf("must:%v, but got: %v", tc.expectedE, e)
 			}
 		})
 	}
@@ -104,23 +104,143 @@ func Test_checkMin_invalidTag(t *testing.T) {
 		Firstname string `json:"firstname" min:"x"`
 	}
 	tt := []struct {
-		name        string
-		t           InvalidTag
-		expectedErr error
+		name      string
+		t         InvalidTag
+		expectedE error
 	}{
 		{
 			name: "should return err when tag value of min is not integer",
 			t: InvalidTag{
 				Firstname: strings.Repeat("e", 9),
 			},
-			expectedErr: errors.New("tag min of field Firstname must be integer, got: x"),
+			expectedE: errors.New("tag min of field Firstname must be integer, got: x"),
 		},
 	}
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			e := checkMin[InvalidTag](tc.t, 0, 0, reflect.ValueOf(&tc.t).Elem())
-			if fmt.Sprintf("%v", tc.expectedErr) != fmt.Sprintf("%v", e) {
-				t.Errorf("must:%v, but got: %v", tc.expectedErr, e)
+			if fmt.Sprintf("%v", tc.expectedE) != fmt.Sprintf("%v", e) {
+				t.Errorf("must:%v, but got: %v", tc.expectedE, e)
+			}
+		})
+	}
+}
+
+func Test_checkMax(t *testing.T) {
+	type Student struct {
+		Firstname string `json:"firstname" max:"5"`
+		Lastname  string `json:"lastname" max:"2"`
+		Age       int    `json:"age"`
+	}
+	tt := []struct {
+		name      string
+		t         Student
+		expectedE error
+	}{
+		{
+			name: "should return nil when struct is valid",
+			t: Student{
+				Firstname: strings.Repeat("e", 5),
+				Lastname:  strings.Repeat("e", 2),
+				Age:       1,
+			},
+			expectedE: nil,
+		},
+		{
+			name: "should return err when Firstname is more than max",
+			t: Student{
+				Firstname: strings.Repeat("e", 6),
+				Lastname:  strings.Repeat("e", 2),
+				Age:       1,
+			},
+			expectedE: errors.New("value of Firstname at row 0 is invalid, value length must less than or equal 5, but got: 6"),
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			e := checkMax[Student](tc.t, 0, 0, reflect.ValueOf(&tc.t).Elem())
+			if fmt.Sprintf("%v", tc.expectedE) != fmt.Sprintf("%v", e) {
+				t.Errorf("must:%v, but got: %v", tc.expectedE, e)
+			}
+		})
+	}
+}
+
+func Test_checkMax_invalidTag(t *testing.T) {
+	type InvalidTag struct {
+		Firstname string `json:"firstname" max:"x"`
+	}
+	tt := []struct {
+		name      string
+		t         InvalidTag
+		expectedE error
+	}{
+		{
+			name: "should return err when tag value of max is not integer",
+			t: InvalidTag{
+				Firstname: strings.Repeat("e", 9),
+			},
+			expectedE: errors.New("tag max of field Firstname must be integer, got: x"),
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			e := checkMax[InvalidTag](tc.t, 0, 0, reflect.ValueOf(&tc.t).Elem())
+			if fmt.Sprintf("%v", tc.expectedE) != fmt.Sprintf("%v", e) {
+				t.Errorf("must:%v, but got: %v", tc.expectedE, e)
+			}
+		})
+	}
+}
+
+func Test_isTagFound(t *testing.T) {
+	type Student struct {
+		Firstname string `min:"1" max:"-5"`
+	}
+	tt := []struct {
+		name      string
+		tag       string
+		t         Student
+		expectedR int
+		expectedE error
+	}{
+		{
+			name: "should return -1 and nil when tag is not found on field",
+			tag:  "other",
+			t: Student{
+				Firstname: strings.Repeat("e", 1),
+			},
+			expectedR: -1,
+			expectedE: nil,
+		},
+		{
+			name: "should return 1 and nil when tag target is min",
+			tag:  "min",
+			t: Student{
+				Firstname: strings.Repeat("e", 1),
+			},
+			expectedR: 1,
+			expectedE: nil,
+		},
+		{
+			name: "should return -1 and err when tag value found but value is a negative",
+			tag:  "max",
+			t: Student{
+				Firstname: strings.Repeat("e", 1),
+			},
+			expectedR: -1,
+			expectedE: errors.New("tag max of field Firstname must more than zero, got: -5"),
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			r, e := isTagFound[Student](tc.t, 0, tc.tag, reflect.ValueOf(&tc.t).Elem())
+			if fmt.Sprintf("%v", tc.expectedE) != fmt.Sprintf("%v", e) {
+				t.Errorf("must:%v, but got: %v", tc.expectedE, e)
+			}
+			if tc.expectedR != r {
+				t.Errorf("must:%v, but got: %v", tc.expectedR, r)
 			}
 		})
 	}
